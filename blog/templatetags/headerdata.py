edf655d5c5a727e7797ffdb76eba1a3b38d491ed
“exrelativity” <exrelativity@gmail.com>
2023-04-10 00:15:58 +0100

    updates

--- blog/templatetags/headerdata.py
+++ blog/templatetags/headerdata.py
@@ -7,18 +7,9 @@ register = template.Library()
 
 @register.filter(name='headerdata')
 def headerdata(request):
-    try:
-        category = [x for x in Category.objects.filter(status=True, front=True)]
-    except Category.DoesNotExist:
-        pass
-    try:
-        subcategory = [x for x in SubCategory.objects.filter(status=True, front=True)]
-    except SubCategory.DoesNotExist:
-        pass
-    try:
-        profile = UsersProfile.objects.get(userId=request.user.id)
-    except UsersProfile.DoesNotExist:
-        pass
+    category = [x for x in Category.objects.filter(status=True, front=True)]
+    subcategory = [x for x in SubCategory.objects.filter(status=True, front=True)]
+    profile = UsersProfile.objects.get(userId=request.user.id)
     return {
         "category": category,
         "subcategory": subcategory,
