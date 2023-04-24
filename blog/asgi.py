65665d3dcd176d7d39c37332a1951ddbffdc50a2
“exrelativity” <exrelativity@gmail.com>
2023-04-24 20:42:53 +0100

    updates

--- blog/asgi.py
+++ blog/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     # Just HTTP for now. (We can add other protocols later.)
